46bfe79a495bc67c0a315502fb0ca301c6208073
Louise Foussat <louisefoussat@gmail.com>
2018-06-20 14:32:57 +0200

    add first component

---
+++ nadar4-app/components/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Homepage = () => {
+  return (
+      <Text>
+        NADAR BB
+      </Text>
+  )
+}
+
+export default Homepage
