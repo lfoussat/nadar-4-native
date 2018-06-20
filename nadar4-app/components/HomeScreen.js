b76237750d5801b72826aecd9c46744ec3291440
Louise Foussat <louisefoussat@gmail.com>
2018-06-20 21:07:28 +0200

    add homepage with gif & fonts & css ok

--- nadar4-app/components/HomeScreen.js
+++ nadar4-app/components/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View, Button, ImageBackground } from 'react-native'
+import { StyleSheet, Text, View, Button, Image, ImageBackground } from 'react-native'
 import { Font } from 'expo'
 import '../assets/fonts/KlinicSlabMedium.otf'
 
@@ -7,11 +7,33 @@ import '../assets/fonts/KlinicSlabMedium.otf'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  scan: {
+    fontFamily: 'KlinicSlabMedium',
+    fontSize: 25,
+    color:'#ffffff',
+    textAlign: 'center',
+    marginTop: 35
   },
   backgroundImage: {
-    flex: 1,
+    flex: 1
+  },
+  image: {
+    marginTop: 28,
+    marginLeft: -8,
+    width: 150,
+    height: 150,
+  },
+  gifLine: {
+    flexWrap: 'wrap',
+    alignItems: 'flex-start',
+    flexDirection:'row',
+  },
+  gif: {
+    width: 100,
+    height: 100
   }
 })
 
@@ -24,6 +46,8 @@ class HomeScreen extends React.Component {
   async componentDidMount() {
     await Font.loadAsync({
       'KlinicSlabMedium': require('../assets/fonts/KlinicSlabMedium.otf'),
+      'KlinicSlabLight': require('../assets/fonts/KlinicSlabLight.otf'),
+      'DroidSans': require('../assets/fonts/DroidSans.ttf'),
     })
     this.setState({ loading: false })
   }
@@ -34,8 +58,17 @@ class HomeScreen extends React.Component {
       : (
       <ImageBackground source={require('../assets/img/fond.png')} style={styles.backgroundImage} >
         <View style={styles.container}>
-          <Text style={{fontFamily: 'KlinicSlabMedium'}}>SCAN YOUR QR CODE</Text>
-          <Button title="Gallery" onPress={() => this.props.navigation.navigate('Gallery')} />
+          <View>
+            <Text style={styles.scan}>SCAN YOUR QR CODE</Text>
+          </View>
+          <View style={{ flex: 1 }}>
+            <Image source={require('../assets/img/qr.png')} style={styles.image} />
+          </View>
+          <View style={styles.gifLine}>
+            <Image source={require('../assets/img/nad-gif.gif')} style={styles.gif} />
+            <Image source={require('../assets/img/paul-gif.gif')} style={styles.gif} />
+            <Image source={require('../assets/img/adri-gif.gif')} style={styles.gif} />
+          </View>
         </View>
       </ImageBackground>
     )
