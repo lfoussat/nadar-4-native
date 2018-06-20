116998c747fd28174c46b37a4c44eb5555fc69ef
Louise Foussat <louisefoussat@gmail.com>
2018-06-20 21:08:19 +0200

    add tabBar and navigation betweek homepage and gallery

--- nadar4-app/App.js
+++ nadar4-app/App.js
@@ -1,22 +1,55 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native'
-import Homepage from './components/Homepage.js'
+import { StyleSheet, Text, View, Button } from 'react-native'
+import HomeScreen from './components/HomeScreen.js'
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation'
+import GalleryScreen from './components/GalleryScreen.js'
+import { Font } from 'expo'
+import './assets/fonts/KlinicSlabMedium.otf'
+
+const RootStack = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {
+      headerVisible: false
+    }
+  },
+  Gallery: GalleryScreen
+})
+
+const RouteTab = createBottomTabNavigator({
+  Home: HomeScreen,
+  Gallery: GalleryScreen,
+}, {
+  tabBarOptions: {
+    style: {
+      backgroundColor: '#333',
+    },
+    labelStyle: {
+      fontFamily: 'KlinicSlabMedium',
+      fontSize: 22
+    },
+    activeTintColor: 'white',
+    inactiveTintColor: 'grey'
+  }
+})
 
 export default class App extends React.Component {
+  state = {
+    loading: true,
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'KlinicSlabMedium': require('./assets/fonts/KlinicSlabMedium.otf'),
+      'KlinicSlabLight': require('./assets/fonts/KlinicSlabLight.otf'),
+      'DroidSans': require('./assets/fonts/DroidSans.ttf'),
+    })
+    this.setState({ loading: false })
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Homepage />
-      </View>
-    );
+    return this.state.loading
+      ? <Text>loading...</Text>
+      : <RouteTab />
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
